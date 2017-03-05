01658e75210ee80b0d80631a677dfb1a962f0b90
195024 <195024@edu.p.lodz.pl>
2017-03-05 19:09:17 +0100

    Lab1 3.1.8

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,11 +2,37 @@ package edu.kis.vh.stacks.list;
 
 class Node {
 
-	int value;
-	Node prev, next;
+	private int value; // w związku z hermetyzacją, w klasie StackList dostęp do 
+						// tego pola jest poprzez getter, a nie bezpośrednio
+	private Node prev;
+	private Node next;
 
 	Node(int i) {
 		value = i;
+	}
+
+	int getValue() {
+		return value;
+	}
+
+	void setValue(int value) {
+		this.value = value;
+	}
+
+	Node getPrev() {
+		return prev;
+	}
+
+	void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	Node getNext() {
+		return next;
+	}
+
+	void setNext(Node next) {
+		this.next = next;
 	}
 
 }
--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -10,9 +10,9 @@ public class StackList {
 		if (last == null)
 			last = new Node(i);
 		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
+			last.setNext(new Node(i));
+			last.getNext().setPrev(last);
+			last = last.getNext();
 		}
 	}
 
@@ -27,14 +27,14 @@ public class StackList {
 	public int peek() {
 		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
-		return last.value;
+		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
-		int ret = last.value;
-		last = last.prev;
+		int ret = last.getValue();
+		last = last.getPrev();
 		return ret;
 	}
 
