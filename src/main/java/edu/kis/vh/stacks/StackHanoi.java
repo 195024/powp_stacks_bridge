8a3841e46f47f202f6552ef5f1c171757536b7f6
195024 <195024@edu.p.lodz.pl>
2017-03-09 22:34:36 +0100

    Lab1 3.3.2

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -1,13 +1,22 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Stos typu 'wieża Hanoi', na którym nie jest możliwe położenie
+ * liczby większej niż obecna na wierzchu stosu.
+ * 
+ */
 public class StackHanoi extends Stack {
-
 	private int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * Kładzie element wejściowy na stosie wykorzystując metodę z klasy bazowej.
+	 * Jeśli element nie spełnia warunku wieży Hanoi, to zostaje odrzucony.
+	 * @param in element, który ma być dołożony na stos
+	 */
 	@Override
 	public void push(int in) {
 		if (!isEmpty() && in > top())
