e313c091da63035070003b3957913a6a593bd6a6
195024 <195024@edu.p.lodz.pl>
2017-03-17 18:43:11 +0100

    Lab2 3.1.13

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -43,3 +43,8 @@ public class Stack {
 }
 // opcja Navigate -> Open Declaration przenosi widok do deklaracji zaznaczonej metody
 // zamiana w punkcie 3.1.6 pozwala na łatwe dobranie implementacji stosu, albo tablica albo lista, zaleta polimorfizmu
+
+// po wykorzystaniu interfejsu w powyższej klasie, funkcja Navigate -> Open Declaration przenosi widok do deklaracji metody w interfejsie
+// Navigate -> Quick Type Hierarchy (ctrl+t) wyświetla podręczne okienko z typami implementującymi lub definującymi zaznaczoną wywoływaną metodą
+// przytrzymanie przycisku Ctrl na zaznaczonym wywołaniu metody wyświetla małe okienko pozwalające albo przejść do deklaracji metody (F3), albo
+// wyświetlić Quick Type Hierarchy (ctrl+t)
